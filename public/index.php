682348b9a8ef378a7d777554a8cb697e90c95789
Will DeBerry <willdeberry@gmail.com>
2017-11-02 10:06:04 -0400

    remove the ip notification

--- public/index.php
+++ public/index.php
@@ -87,17 +87,17 @@ if ($config['memcache']['enabled'] && $config['mc_antidos']['enabled']) {
 
 // Got past rate limiter and session manager
 // show last logged in popup if it's still set
-if (@$_GET['clp'] == 1 && @$_SESSION['last_ip_pop']) unset($_SESSION['last_ip_pop']);
-if (count(@$_SESSION['last_ip_pop']) == 2) {
-  $data = $_SESSION['last_ip_pop'];
-  $ip = filter_var($data[0], FILTER_VALIDATE_IP);
-  $time = date("l, F jS \a\\t g:i a", $data[1]);
-  if (@$_SESSION['AUTHENTICATED'] && $_SESSION['last_ip_pop'][0] !== $user->getCurrentIP()) {
-    $_SESSION['POPUP'][] = array('CONTENT' => "You last logged in from <b>$ip</b> on $time", 'DISMISS' => 'yes', 'ID' => 'lastlogin', 'TYPE' => 'alert alert-warning');
-  } else {
-    $_SESSION['POPUP'][] = array('CONTENT' => "You last logged in from <b>$ip</b> on $time", 'DISMISS' => 'yes', 'ID' => 'lastlogin', 'TYPE' => 'alert alert-info');
-  }
-}
+//if (@$_GET['clp'] == 1 && @$_SESSION['last_ip_pop']) unset($_SESSION['last_ip_pop']);
+//if (count(@$_SESSION['last_ip_pop']) == 2) {
+//  $data = $_SESSION['last_ip_pop'];
+//  $ip = filter_var($data[0], FILTER_VALIDATE_IP);
+//  $time = date("l, F jS \a\\t g:i a", $data[1]);
+//  if (@$_SESSION['AUTHENTICATED'] && $_SESSION['last_ip_pop'][0] !== $user->getCurrentIP()) {
+//    $_SESSION['POPUP'][] = array('CONTENT' => "You last logged in from <b>$ip</b> on $time", 'DISMISS' => 'yes', 'ID' => 'lastlogin', 'TYPE' => 'alert alert-warning');
+//  } else {
+//    $_SESSION['POPUP'][] = array('CONTENT' => "You last logged in from <b>$ip</b> on $time", 'DISMISS' => 'yes', 'ID' => 'lastlogin', 'TYPE' => 'alert alert-info');
+//  }
+//}
 
 // Create our pages array from existing files
 if (is_dir(INCLUDE_DIR . '/pages/')) {
